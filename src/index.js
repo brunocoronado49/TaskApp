6a3fc302ba9669f02348ace22b5995cb6bd0d026
brunocoronado49 <jofrancoracor@gmail.com>
2021-12-12 12:47:56 -0600

    Configurando la ruta inicial

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import express from "express";
+import indexRoutes from './routes/indexRoute'
 
 const app = express();
+
+app.use(indexRoutes)
 
 app.listen(3000);
 console.log("Server on port: ", 3000);
