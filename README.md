839851a097e5b9a2e72620c534acfc3e24596b67
brunocoronado49 <jofrancoracor@gmail.com>
2021-12-13 15:00:53 -0600

    Proyecto terminado con su version compilada

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+### For init the proyect
+
 1. npm init --yes
 2. Create commands for run the app in packaje.json -> scripts
 3. Install dependencies
@@ -6,3 +8,4 @@
 6. for execute the file index.js is: npx babel-node src/index.js
 7. Change the config for include babel-node in the scripts and execute
 8. Continue whit the code
+9. Add environment variables (MONGODB_URI)
--- src/database/database.js
+++ src/database/database.js
@@ -1,8 +1,9 @@
 import mongoose from 'mongoose'
+import { MONGODB_URI } from '../config/config'
 
 (async () => {
     try {
-        const database = await mongoose.connect('mongodb://localhost/taskdb')
+        const database = await mongoose.connect(MONGODB_URI)
         console.log('Database connected in:', database.connection.name)
     } catch (error) {
         console.log(error)
