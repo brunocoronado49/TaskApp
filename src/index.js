70dfc804708bbb1db656cba7f1a3479a0d89f3e8
brunocoronado49 <jofrancoracor@gmail.com>
2021-12-13 17:55:27 -0600

    Cambiando de puerto

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import app from './app'
 import './database/database'
 
-app.listen(3000);
-console.log("Server on port: ", 3000);
+app.listen(process.env.PORT || 3000);
+console.log("Server on port: ", process.env.PORT);
